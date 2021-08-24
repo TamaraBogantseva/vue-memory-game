6dc298bfd4e854735c046e194ce116d94a8d09f7
Tamara <t9997980976@gmail.com>
2021-08-24 13:53:06 +0200

    create cards template

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <h1>Memory Game</h1>
+  <div class="board">
+    <Card v-for="(card, index) in cardsList" :key="`card-${index}`" />
+  </div>
 </template>
 
 <script>
-
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
+    Card,
+  },
 
-  }
-}
+  setup() {
+    const cardsList = [];
+    for (let i = 0; i < 16; i++) {
+      cardsList.push(i);
+    }
+    return {
+      cardsList,
+    };
+  },
+};
 </script>
 
 <style>
@@ -21,5 +34,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.board {
+  display: grid;
+  grid-template-columns: 150px 150px 150px 150px;
+  grid-template-rows: 150px 150px 150px 150px;
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+"<template>
+  <div class="card">
+    <div class="card-side front" v-if="visible">Front</div>
+    <div class="card-side back" v-else>Back</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style>
+.card {
+  width: 100%;
+  height: 100%;
+  border: 1px solid #000;
+  position: relative;
+}
+.card-side {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.card-side.back {
+  background-color: rgb(74, 230, 74);
+}
+.card-side.front {
+  background-color: rgb(0, 187, 255);
+}
+</style>
